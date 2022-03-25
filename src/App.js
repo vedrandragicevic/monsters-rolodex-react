2ae6e42fd09ee67cc5b13eda52ddde9346a1a01c
vedrandragicevic <vedrandx@gmail.com>
2022-03-25 16:18:55 +0100

    converted main app class component to function component

--- src/App.js
+++ src/App.js
@@ -1,57 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import CardList from './components/card-list/card-list.component.jsx';
 import SearchBox from './components/search-bar/search-box.component.jsx';
 
 
-class App extends Component {
-  constructor() {
-    super();
 
-    this.state = {
-      monsters: [],
-      searchField: ''
-    };
-    console.log('constructor');
-  }
+const App = () => {
 
-  // Mount = First time a compoment is rendered, Use this for API Fetch
-  componentDidMount() {
-    console.log('componentDidMount')
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then((users) => this.setState( () => {
-          return {monsters: users}
-        },
-         () => {
-           console.log(this.state);
-         }
-      )
-    );
-  }
+    .then((users) => setMonsters(users));
+  }, []);
 
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();         
-     this.setState( () => {
-      return { searchField };
-  });
-}
+  useEffect(() => {
+
+    const newFilteredMonsters = monsters.filter( (monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
 
-  render() {
-    console.log('render');
+  const onSearchChange = (event) => {   
+      const searchFieldString = event.target.value.toLocaleLowerCase(); 
+      setSearchField(searchFieldString);
+  };
 
-    const { monsters, searchField, } = this.state;
-    const { onSearchChange } = this;
+  
 
-    const filteredMonsters = monsters.filter( (monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchField);
-          });
 
-    return (
-      <div className="App">
+  return (
+    <div className="App">
         <h1 className='app-title'>Monsters Rolodex</h1>
 
         <SearchBox 
@@ -62,8 +50,8 @@ class App extends Component {
         <CardList monsters={filteredMonsters} />
    
       </div>
-    );
-  }
-}
+  );
+};
+
 
 export default App;
